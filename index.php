3e907113db971bb4a9b77249e39c44c0fa779a1e
soura4u <64581748+soura4u@users.noreply.github.com>
2020-05-07 14:46:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 	<div class="container">
 		<div class="index">
 			<div class="indextext">
-				We help you to control your budget
+				We help you to plan your budget
 			</div><br>
 			<div>
 				<a href="login.php" class="btn btn-info">Start Today <i class="fas fa-arrow-circle-right"></i></a>
