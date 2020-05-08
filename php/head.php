d24c45844fb84948dcdd1417ec9304c232a13a20
soura4u <64581748+soura4u@users.noreply.github.com>
2020-05-08 22:15:25 +0530

    gg

--- php/head.php
+++ php/head.php
@@ -1,8 +1,11 @@
 <?php $ver="09-05-2020"; ?>
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css?<?php echo $ver; ?>">
 	<link rel="shortcut icon" type="image/jpg" href="icon/icon.png"/>
 	<link rel="stylesheet" href="fontawesome/css/all.min.css">
+	<script src="js/wow.min.js"></script>
+    <script>new WOW().init();</script>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 	<script type="text/javascript">
